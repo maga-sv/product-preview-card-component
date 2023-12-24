64b572de025e9fafcb57911a53b1ac9421f75c9b
Muhammadaziz <salohiddinov.mm@gmail.com>
2023-12-24 18:04:23 +0500

    price changed

--- src/stores/products.js
+++ src/stores/products.js
@@ -9,7 +9,7 @@ export const useProductsStore = defineStore("products", {
                 category: 'perfume',
                 info: 'A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.',
                 price: 169.99,
-                discount: 0.1171,
+                discount: 0.1177,
             }
         ]
     })
